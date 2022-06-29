e459d3b86ba37d2b0434caea8d639bc1d676d335
Manoj Fernando <manojf@99x.lk>
2022-06-29 07:25:11 +0530

    New vct env config

--- web/src/environments/environment.prod.ts
+++ web/src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   host: 'https://vct.winningproduct.com/vct',
   loginUrl: 'https://vct.auth.ap-south-1.amazoncognito.com/' +
-  'login?response_type=code&client_id=7iql37q16ckf38022qesmk809r&redirect_uri=https://vct.winningproduct.com',
+  'login?response_type=code&client_id=7iql37q16ckf38022qesmk809r&redirect_uri=https://vct.winningproduct.com/auth',
   redirectSignIn: 'https://vct.winningproduct.com/auth',
   redirectSignOut: 'https://vct.winningproduct.com/auth'
 };
